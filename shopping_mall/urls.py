632fe66a4a5e36262da9b1c40be579065ffa0ec6
“yujunxionggit config --global user.email “2140585762@qq.com <2140585762@qq.com>
2022-03-20 10:08:22 +0800

    完成商品详情页的书写

--- shopping_mall/urls.py
+++ shopping_mall/urls.py
@@ -7,4 +7,5 @@ app_name = 'shopping'
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('search/', views.search, name='search'),
+    path('details/', views.shop_details, name='shop_details'),
 ]
--- shopping_mall/views.py
+++ shopping_mall/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render, HttpResponse
 
 
 # Create your views here.
-
+# 商店主页
 def home(request):
     return render(request, 'shopping/home.html')
 
 
+# 商店搜索结果展示页
 def search(request):
     return render(request, 'shopping/search.html')
+
+
+# 商品详情页
+def shop_details(request):
+    return render(request, 'shopping/details_shop.html')
